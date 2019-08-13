13bc14b1c153850e46552401846fd008ec8dccf4
Eunjong Kim <eunjongkim@github.com>
2019-08-12 21:25:14 -0700

    added balancing pulse option. Not tested yet

--- Painter_PulseCorrection/Painter_PulseCorrection.py
+++ Painter_PulseCorrection/Painter_PulseCorrection.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('C:\\Program Files (x86)\\Labber\\Script')
+
 import Labber
 import numpy as np
 from scipy.interpolate import interp1d
@@ -14,7 +15,7 @@ class Driver(InstrumentDriver.InstrumentWorker):
         """Perform the operation of opening the instrument connection"""
         self.is_uniform_transfer_func = self.getValue("Uniform Transfer Function")
         self.nTraces = ndict[self.getValue('Number of Traces to Correct')]
-
+        self.is_add_balancing_pulse = bool(self.getValue("Add Balancing Pulse"))
         # store list of input traces
         self.input_traces = []
         for n in range(self.nTraces):
@@ -25,15 +26,37 @@ class Driver(InstrumentDriver.InstrumentWorker):
         self.transfer_funcs = []
         if self.is_uniform_transfer_func:
             dpath = self.getValue('Transfer Function - Path')
-            self.transfer_funcs.append(__transfer_func(dpath))
+            self.transfer_funcs.append(self._transfer_func(dpath))
         else:
             for n in range(self.nTraces):
                 dpath = self.getValue('Transfer Function - Path #' + str(n + 1))
-                self.transfer_funcs.append(__transfer_func(dpath))
+                self.transfer_funcs.append(self._transfer_func(dpath))
 
-    def __append_balancing_pulse(self, trace_idx):
+    def _append_balancing_pulse(self, trace_idx):
+        start_delay = self.getValue("Balancing Pulse - Start Delay")
+        duration = self.getValue("Balancing Pulse - Duration")
+        end_delay = self.getValue("Balancing Pulse - End Delay")
 
-    def __transfer_func(path):
+        trace_in = self.input_traces[trace_idx]
+        y_in, dt = trace_in['y'], trace_in['dt']
+
+        # number of points corresponding to sections of balancing pulse
+        n_start_delay = int(np.round(start_delay / dt))
+        n_duration = int(np.round(duration / dt))
+        n_end_delay = int(np.round(end_delay / dt))
+
+        area = np.sum(y_in) * dt
+        # amplitude of the balancing pulse
+        amplitude = - area / (n_duration * dt)
+
+        # append start delay, balancing pulse, and the end delay
+        y_in = np.append(y_in, np.zeros(n_start_delay))
+        y_in = np.append(y_in, amplitude * np.ones(n_duration))
+        y_in = np.append(y_in, np.zeros(n_end_delay))
+
+        trace_in['y'] = y_in
+
+    def _transfer_func(self, path):
         """
         Construct transfer function from interpolation of data specified by path
         """
@@ -57,8 +80,7 @@ class Driver(InstrumentDriver.InstrumentWorker):
             value = self.getValue("Input Trace #" + str(n + 1))
             self.log("Trace In #" + str(n + 1) + " = " + str(value['y']))
             self.input_traces[n] = value
-
-            # TODO: balancing pulse
+            self._append_balancing_pulse(n)
 
         if quant.name.startswith('Output Trace'):
             n = int(quant.name.split(' #')[1]) - 1
@@ -70,20 +92,20 @@ class Driver(InstrumentDriver.InstrumentWorker):
 
             trace_in = self.input_traces[n]
             y_in = trace_in['y']
-            dt_in = trace_in['dt']
-            t_in = np.array([trace_in['t0'] + m * dt_in for m in range(len(trace_in['y'])))
+            dt = trace_in['dt']
+            t_in = np.array([trace_in['t0'] + m * dt for m in range(len(trace_in['y'])))
 
             # perform deconvolution of the trace if the vector has at least 2 entries
             if len(y_in) > 1:
                 Y_in = np.fft.rfft(y_in, norm='ortho')
-                ω = 2 * np.pi * np.fft.rfftfreq(y_in, d=dt_in)
+                ω = 2 * np.pi * np.fft.rfftfreq(y_in, d=dt)
 
                 # deconvolution
-                Y_out = Y_in / H
+                Y_out = Y_in / H(ω)
                 y_out = np.fft.irfft(Y_out, norm='ortho')
 
                 self.log("Trace Out #" + str(n + 1) + " = " + str(y_out))
-                value = quant.getTraceDict(y_out, t0=t_in[0], dt=dt_in)
+                value = quant.getTraceDict(y_out, t0=t_in[0], dt=dt)
         return value
 
 if __name__ == '__main__':
