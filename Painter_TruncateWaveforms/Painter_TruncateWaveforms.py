da87c27ee72e9fe09bbd0ca50b3dfbcc81a9a5c4
Eunjong Kim <ekim7206@gmail.com>
2019-09-16 18:17:48 -0700

    added option to allow for different truncation times for differnt traces, in case there is a physical delay between channels

--- Painter_TruncateWaveforms/Painter_TruncateWaveforms.py
+++ Painter_TruncateWaveforms/Painter_TruncateWaveforms.py
@@ -16,24 +16,25 @@ class Driver(InstrumentDriver.InstrumentWorker):
         if quant.name.startswith('Output Trace'):
             n = int(quant.name.split(' #')[1]) - 1
 
-            if self.getValue("Uniform Input Sample Rate"):
-                dt = 1 / self.getValue("Sample Rate")
+            dt = 1 / self.getValue("Sample Rate")
 
             trace_in = self.getValue("Input Trace #" + str(n + 1))
             self.log("Trace In #" + str(n + 1) + " = " + str(trace_in['y']))
 
             t_first_delay = self.getValue("First Pulse Delay")
-            t_trunc = t_first_delay + self.getValue("Truncation Time")
+            t_trunc = t_first_delay + \
+                self.getValue("Truncation Time #" + str(n + 1))
 
             n_trunc = int(np.round(t_trunc / dt))
 
-            trace_out = np.copy(trace_in)
-            # zero the waveform from (n_trunc + 1)-th point
-            trace_out[n_trunc:] = np.zeros(len(trace_in) - n_trunc)
-            # perform interpolation of the data if the vector has at least 2 entries
+            if len(trace_in['y']) > 1:
+                trace_out = np.copy(trace_in['y'])
+                # zero the waveform from (n_trunc + 1)-th point
+                trace_out[n_trunc:] = np.zeros(len(trace_in['y']) - n_trunc)
+                # perform interpolation of the data if the vector has at least 2 entries
 
-            t_in = np.array([trace_in['t0'] + m * dt for m in range(len(trace_in['y']))])
-            value = quant.getTraceDict(trace_out, t0=t_in[0], dt=dt)
+                t_in = np.array([trace_in['t0'] + m * dt for m in range(len(trace_in['y']))])
+                value = quant.getTraceDict(trace_out, t0=t_in[0], dt=dt)
 
         return value
 
